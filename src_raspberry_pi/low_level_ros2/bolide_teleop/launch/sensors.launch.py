9c7c2fde269c1d0d0e24294dbaf536588f768d2b
SharaineMALARVIJY <sharaine.ma@gmail.com>
2026-03-21 22:23:38 +0100

    add sensors launch file

---
+++ src_raspberry_pi/low_level_ros2/bolide_teleop/launch/sensors.launch.py
@@ -0,0 +1,122 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.actions import EmitEvent
+from launch.events import Shutdown
+
+restart_on_error = False
+
+def generate_launch_description():
+    sllidar = Node(
+        package="sllidar_ros2",
+        executable="sllidar_node",
+        name="sllidar_node",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[
+            {"serial_port": "/dev/ttyLIDAR"},
+            {"serial_baudrate": 256000},
+            {"frame_id": "laser_frame"},
+            {"inverted": False},
+            {"angle_compensate": True},
+            {"scan_mode": "Express"},
+        ]
+    )
+    stm32_node = Node(
+        package="bolide_stm32",
+        executable="stm32_node",
+        name="stm32_node",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[{"debug": False}],
+    )
+    cmd_vel_node = Node(
+        package="bolide_stm32",
+        executable="cmd_vel_node",
+        name="cmd_vel_node",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[{"debug": False},
+                    {'cmd_vel_deadzone': 0.01},
+                    {'pwm_forward_max': 2000},  # <= 2000 (ESC max)
+                    {'pwm_forward_min': 1540},
+                    {'pwm_reverse_min': 1460},
+                    {'pwm_reverse_max': 1000}], # >= 1000 (ESC min)
+    )
+    cmd_vel_bridge_node = Node(
+        package="bolide_stm32",
+        executable="cmd_vel_bridge_node",
+        name="cmd_vel_bridge_node",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[{"debug": False}],
+    )
+    cmd_dir_node = Node(
+        package="bolide_direction",
+        executable="cmd_dir_node",
+        name="cmd_dir_node",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[{"debug": False},
+                    {'baudrate': 1000000},
+                    {'max_steering_angle': 21.0},       # steering angle de chaque côté (en °) -ne correspond pas exactement à l'angle réel-
+                    {'steering_offset_deg': -1.5}],     # offset (sens marche avant) : gauche >0, droit <0 (en°)
+    )
+    odom_node = Node(
+        package="bolide_stm32",
+        executable="odom_node",
+        name="odom_node",
+        output="screen",
+    )
+    tf_base_to_link = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="tf_base_to_link",
+        arguments=["0", "0", "0", "0", "0", "0", "base_footprint", "base_link"]
+    )
+
+    tf_link_to_laser = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="tf_link_to_laser",
+        arguments=["0.18", "0", "0.10", "3.14159265359", "0", "0", "base_link", "laser_frame"]
+    )
+    teleop_keyboard = Node(
+        package="bolide_teleop",
+        executable="teleop_keyboard",
+        name="teleop_keyboard",
+        output="screen",
+        respawn=restart_on_error,
+        parameters=[
+            {'debug': False},
+            {'speed_increment': 0.01},
+            {'direction_increment': 0.2},
+            {'rate': 10}                        # Hz
+        ]
+    )
+
+
+    # Quand teleop_keyboard se termine → shutdown de tout le launch
+    shutdown_on_teleop_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=teleop_keyboard,
+            on_exit=[EmitEvent(event=Shutdown())]
+        )
+    )
+
+
+    return LaunchDescription(
+        [
+            odom_node,
+            tf_base_to_link,
+            tf_link_to_laser,
+            sllidar,
+            stm32_node,
+            cmd_vel_node,
+            cmd_dir_node,
+            cmd_vel_bridge_node,
+            #teleop_keyboard,
+            shutdown_on_teleop_exit,
+        ]
+    )
--- src_raspberry_pi/low_level_ros2/bolide_teleop/setup.py
+++ src_raspberry_pi/low_level_ros2/bolide_teleop/setup.py
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'sensors=bolide_teleop.sensors:main',
             'teleop_keyboard=bolide_teleop.teleop_keyboard:main',
         ],
     },
